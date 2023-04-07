e6dca9c24c31f774c76083df989c43e8d87accc7
arsha-s <arsha13613@gmail.com>
2023-04-07 19:45:16 +0530

    Code updated to add different kinds of data binding

--- Simple tasks/Checking validity of a number/src/app/app.component.ts
+++ Simple tasks/Checking validity of a number/src/app/app.component.ts
@@ -6,13 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //Variables
   value: number = 0;
   isValid: boolean = false;
   checked: boolean = false;
+
+  //Function to check validity of the user input.
   checkValidity(value: number){
     this.checked = true;
-    if (value>=0 && value<=100)
+    if (value>=1 && value<=100)
       this.isValid = true;
     else this.isValid = false;
+  }
+
+  //Function to reset the input field
+  reset(){
+    this.value = 0;
+    this.checked = false;
   }
 }
