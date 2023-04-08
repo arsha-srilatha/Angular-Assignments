5afd499a8df824f9ed09758555336d13414302ac
arsha-s <arsha13613@gmail.com>
2023-04-08 14:02:41 +0530

    Code updated to use different built-in directives

--- Simple tasks/Checking validity of a number/src/app/app.component.ts
+++ Simple tasks/Checking validity of a number/src/app/app.component.ts
@@ -10,12 +10,17 @@ export class AppComponent {
   value: number = 0;
   isValid: boolean = false;
   checked: boolean = false;
+  values: any = []; //array to store the values provided by user in a single session
+  showValues: boolean = false;
 
   //Function to check validity of the user input.
   checkValidity(value: number){
     this.checked = true;
-    if (value>=1 && value<=100)
+    if (value>=1 && value<=100){
       this.isValid = true;
+      this.values.push(value);
+      console.log(this.values);
+    } 
     else this.isValid = false;
   }
 
@@ -23,5 +28,14 @@ export class AppComponent {
   reset(){
     this.value = 0;
     this.checked = false;
+  }
+
+  /*Function to display the list of valid values entered by user.
+  Also acts as a function to go back to page that accepts user values */
+  showValuesArray(){
+    this.showValues = !this.showValues
+    if (this.showValues) 
+      return 'Enter values';
+    else return 'Show values';
   }
 }
