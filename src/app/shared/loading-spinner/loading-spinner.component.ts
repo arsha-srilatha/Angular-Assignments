d43bbf387cf7bef1376339b9e8de909fec6fd5e7
arsha-srilatha <127752402+arsha-srilatha@users.noreply.github.com>
2024-03-14 21:52:33 +0530

    Delete src directory

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading-spinner',
-  template: '<span class="loader"></span>',
-  styleUrls: ['./loading-spinner.component.css'],
-})
-export class LoadingSpinnerComponent {}
